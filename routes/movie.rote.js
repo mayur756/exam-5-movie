6df44ba33b010289870e9655a3f39284efad5310
mayur756 <147247475+mayur756@users.noreply.github.com>
2024-09-30 09:13:06 +0530

    movie

--- routes/movie.rote.js
+++ routes/movie.rote.js
@@ -5,7 +5,7 @@ const movieRouter = Router();
 
 movieRouter.get('/allmovie',allmovies);
 movieRouter.post('/createmovie',createMovie);
-movieRouter.patch('update',updateMovie);
-movieRouter.delete('deleteMovie',deleteMovie);
+movieRouter.patch('/update/:id',updateMovie);
+movieRouter.delete('/deleteMovie/:id',deleteMovie);
 
 module.exports = movieRouter;
